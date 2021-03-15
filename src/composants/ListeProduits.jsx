8d788159bf26a0d001f58e0bbbf14eccd02af0dc
QcDiablo <74427389+QcDiablo@users.noreply.github.com>
2021-03-15 09:15:15 -0400

    Remise exercice 3

--- src/composants/ListeProduits.jsx
+++ src/composants/ListeProduits.jsx
@@ -3,12 +3,12 @@ import Produit from "./Produit";
 import { useEffect, useState } from 'react';
 /******* Ex#3 - Étape D ********************************/ 
 // Importer l'objet bd du fichier firebase.js
-
+import bd from '../data/firebase';
 
 export default function ListeProduits(props) {
   /******* Ex#3 - Étape E ********************************/ 
   // Créer un "état" React pour les produits (utiliser useState)
-  
+  const [produits, setProduits] = useState([]);
     
   useEffect(() => {
     async function getProduits() {
@@ -20,14 +20,20 @@ export default function ListeProduits(props) {
       // [Suggestion : remarquez que la fonction getProduits() est marquée 'async'. Lorsque vous appelez la méthode Firestore qui retourne les produits, cette fonction 
       // est une Promesse, vous pouvez simplement utiliser la syntax 'await' pour attendre le résultat avant de remplir le tableau tabProduits 
       // (visionnez la capsule au sujet du code asynchrone en JavaScript)]
-
+      const reponse = await bd.collection('ex3-produits').get();
       
       /******* Ex#3 - Étape G ********************************/ 
       // Modifier l'état des produits (initialisé ci-dessus avec useState) en utilisant le mutateur et le tableau tabProduits
-      
+      reponse.forEach(doc => tabProduits.push({
+        id: doc.id,
+        ...doc.data()
+      }));
+      setProduits(tabProduits);
     }
     getProduits();
   }, []); // Ne modifiez surtout pas le tableau des dépendances à gauche : vous risquez un appel récurent sans fin de l'API Firebase !!!!
+
+  console.log(produits);
 
   return (
     <div className="ListeProduits">
@@ -41,7 +47,9 @@ export default function ListeProduits(props) {
           avoir l'attribut "etatPanier={props.etatPanier}" quand vous les générer ici : encore une fois, regardez 
           le code de l'exercice de classe.
         */}
-
+        {produits.map( 
+          produit => <Produit etatPanier={props.etatPanier} {...produit} key={produit.id} />
+        )}
       </ul>
     </div>
   );
--- src/data/firebase.js
+++ src/data/firebase.js
@@ -5,6 +5,11 @@ import 'firebase/firestore';
 // Ajouter votre objet de configuration Firebase
 const firebaseConfig = {
   
+  
+  
+  
+  
+  
 };
 
 // Initialiser Firebase
@@ -16,3 +21,4 @@ if(!fbAppli.apps.length) {
 const bd = fbAppli.firestore();
 /******* Ex#3 - Étape C ********************************/ 
 // Exporter (par défaut) la référence à "bd" pour pouvoir l'utiliser dans le composant ListeProduits
+export default bd;
